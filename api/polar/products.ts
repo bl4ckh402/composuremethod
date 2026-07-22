6bd165e5f7a843f43f62b3573fd0deb98d1e7c64
bl4ckh401 <pavkiptoo@gmail.com>
2026-07-22 22:30:42 +0300

    fix preview pages

--- api/polar/products.ts
+++ api/polar/products.ts
@@ -1,4 +1,4 @@
-import { getPolarClient } from '../src/lib/polar';
+import { getPolarClient } from '../../src/lib/polar';
 
 export default async (req: any, res: any) => {
   if (req.method !== 'GET') {
