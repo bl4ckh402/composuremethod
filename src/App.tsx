7ce1954eca380c9fd049c9bc22e1e876b6bc1f74
bl4ckh401 <pavkiptoo@gmail.com>
2026-07-24 15:03:27 +0300

    fix vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { ViewMode } from './types';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
@@ -254,6 +255,7 @@ export default function App() {
       {!isMobile && currentView === 'home' && (
         <Footer onNavigate={handleNavigate} onOpenCheckout={handleOpenCheckout} />
       )}
+      <Analytics />
     </div>
   );
 }
